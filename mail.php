e79661188d45d3f7116ea3b3aab7e41afc977bec
toz7 <48638110+toz7@users.noreply.github.com>
2022-10-04 10:06:33 -0400

    Update mail.php

--- mail.php
+++ mail.php
@@ -1,39 +1,25 @@
-<?php
+<?php 
+    $message_sent = false;
+    if(isset($_POST['email']) && $_POST['email'] != '') {
+        if(filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) ) {
 
-$name = $_POST['name'];
-$email = $_POST['email'];
-$subject = $_POST['subject'];
-$message = $_POST['message'];
+            $userName = $_POST['name'];
+            $userEmail = $_POST['email'];
+            $messageSubject = $_POST['subject'];
+            $message = $_POST['message'];
 
-$mailheader = "From:".$name."<".$email.">\r\n";
+            $to = "toz7@live.com";
+            $body = "";
 
-$recipient = "toz7@live.com";
+            $body .= "From: ".$userName. "\r\n";
+            $body .= "Email: ".$userEmail. "\r\n";
+            $body .= "Message: ".$message. "\r\n";
 
-mail($recipient, $subject, $message, $mailheader) or die("Error!");
-
-echo' 
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Contact form</title>
-    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@600&family=Poppins&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="style.css">
-</head>
-<body>
-    <div class="container">
-        <h1>Thank you for contacting me. I will get back to you as soon as possible!</h1>
-        <p class="back">Go back to the <a href="index.html">homepage</a>.</p>
-        
-    </div>
-</body>
-</html>
-
-
-';
-
+            mail($to, $messageSubject, $body);
+            
+            $message_sent = true;
+        }
+    }
+    
 ?>
 
